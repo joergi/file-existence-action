1b8878818b00ee4cbe23983d83c188b8b01cd84e
joergi <1439809+joergi@users.noreply.github.com>
2020-10-16 14:39:37 +0200

    adds dependabot to the project, so you have up to date dependencies
    test works with folder in lowercase

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,22 +4,22 @@ import {expect} from "chai";
 describe('call checkExistence', function() {
 
         it('should return true for existing file', async function () {
-            const res = await checkExistence("src/test/resource/TestFolder/test.txt", false);
+            const res = await checkExistence("src/test/resource/testfolder/test.txt", false);
             expect(res).to.equal(true);
         });
 
         it('should return false for uppercase file', async function () {
-        const res = await checkExistence("src/test/resource/TestFolder/TEST.txt", false);
+        const res = await checkExistence("src/test/resource/testfolder/TEST.txt", false);
         expect(res).to.equal(false);
         });
 
         it('should return true for uppercase file with ignoreCase true', async function () {
-            const res = await checkExistence("src/test/resource/TestFolder/TEST.txt", true);
+            const res = await checkExistence("src/test/resource/testfolder/TEST.txt", true);
             expect(res).to.equal(true);
         });
 
         it('should return true for mIxEd case file with ignoreCase true', async function () {
-            const res = await checkExistence("src/test/resource/TestFolder/TeSt.txt", true);
+            const res = await checkExistence("src/test/resource/testfolder/TeSt.txt", true);
             expect(res).to.equal(true);
         });
 
