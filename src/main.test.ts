d9c6141de852764099fbb110941002eae45c47b2
joergi <1439809+joergi@users.noreply.github.com>
2020-10-16 15:43:52 +0200

    adds dependabot to the project, so you have up to date dependencies
    fixing linter / prettier issues

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,30 +1,40 @@
-import {checkExistence} from "./main";
-import {expect} from "chai";
+import {checkExistence} from './main'
+import {expect} from 'chai'
 
 describe('call checkExistence', function() {
+  it('should return true for existing file', async function() {
+    const res = await checkExistence(
+      'src/test/resource/testfolder/test.txt',
+      false
+    )
+    expect(res).to.equal(true)
+  })
 
-        it('should return true for existing file', async function () {
-            const res = await checkExistence("src/test/resource/testfolder/test.txt", false);
-            expect(res).to.equal(true);
-        });
+  it('should return false for uppercase file', async function() {
+    const res = await checkExistence(
+      'src/test/resource/testfolder/TEST.txt',
+      false
+    )
+    expect(res).to.equal(false)
+  })
 
-        it('should return false for uppercase file', async function () {
-        const res = await checkExistence("src/test/resource/testfolder/TEST.txt", false);
-        expect(res).to.equal(false);
-        });
+  it('should return true for uppercase file with ignoreCase true', async function() {
+    const res = await checkExistence(
+      'src/test/resource/testfolder/TEST.txt',
+      true
+    )
+    expect(res).to.equal(true)
+  })
 
-        it('should return true for uppercase file with ignoreCase true', async function () {
-            const res = await checkExistence("src/test/resource/testfolder/TEST.txt", true);
-            expect(res).to.equal(true);
-        });
-
-        it('should return true for mIxEd case file with ignoreCase true', async function () {
-            const res = await checkExistence("src/test/resource/testfolder/TeSt.txt", true);
-            expect(res).to.equal(true);
-        });
-
-});
+  it('should return true for mIxEd case file with ignoreCase true', async function() {
+    const res = await checkExistence(
+      'src/test/resource/testfolder/TeSt.txt',
+      true
+    )
+    expect(res).to.equal(true)
+  })
+})
 
 afterAll(done => {
-    done()
+  done()
 })
